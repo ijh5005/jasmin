82c8db8cf89f9a5eb188c24f53554dbdcd8dfff6
Isaiah Harrison <isaiahharrison@f0-18-98-15-42-a5.wireless.temple.edu>
2018-09-14 19:10:48 -0400

    - video added

--- src/components/LeftSide.js
+++ src/components/LeftSide.js
@@ -25,11 +25,9 @@ class LeftSide extends Component {
                 </a>
               </div>
               <div className="currentVideoBox">
-
                 <div className="videoBox show">
-
+                  <iframe width="100%" height="100%" src="https://www.youtube.com/embed/GNWriGwPdq8" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
                 </div>
-
               </div>
               <div className="videoGalleryBox">
 
